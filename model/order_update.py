88a0b48c1baea10358d79344b7c7c90df88bad76
yite <coastq22889@icloud.com>
2023-12-18 23:34:12 +0800

    from binance

--- model/order_update.py
+++ model/order_update.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Tuple
 
 
 @dataclass
@@ -12,7 +12,30 @@ class OrderUpdate:
     ts: int
     u: int
     pu: int
-    a: List[float, float]
-    b: List[float, float]
+    a: List[Tuple[float, float]]
+    b: List[Tuple[float, float]]
+
+    @classmethod
+    def from_binance(cls, data: dict):
+        symbol = data['s']
+        base = symbol[:-4]  # Assuming the last 4 characters are the quote symbol
+        quote = symbol[-4:]
+
+        a_list = [(float(price), float(quantity)) for price, quantity in data['a']]
+        b_list = [(float(price), float(quantity)) for price, quantity in data['b']]
+
+        return cls(
+            id=data['E'],
+            ex="binance",
+            base=base,
+            quote=quote,
+            inst="spot",
+            ts=data['T'],
+            u=data['u'],
+            pu=data['pu'],
+            a=a_list,
+            b=b_list
+        )
+
 
 
