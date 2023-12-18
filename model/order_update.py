68ec41a2058e94b821c82dab4f5f8771018ee545
yite <coastq22889@icloud.com>
2023-12-18 23:25:14 +0800

    add order update

--- model/order_update.py
+++ model/order_update.py
@@ -1,8 +1,18 @@
 from dataclasses import dataclass
+from typing import List
 
 
 @dataclass
 class OrderUpdate:
+    id: int
+    ex: str
     base: str
+    quote: str
+    inst: str
+    ts: int
+    u: int
+    pu: int
+    a: List[float, float]
+    b: List[float, float]
 
 
